68c768324dd38a7430deb7d546adc9fb66f3a6a7
keepo-dot <bexel96@gmail.com>
2025-04-10 19:01:56 -0400

    Started SSG. :)

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from textnode import TextNode
+
+from textnode import TextType
+
+
+
+
+def main():
+    textnode_object = TextNode("This is some text.", TextType.NORMAL_TEXT, )
+    print(textnode_object)
+
+
+
+    return
+
+
+main()
+
+
+
+
+
---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "Normal Text"
+    BOLD_TEXT = "Bold Text"
+    ITALIC_TEXT = "Italic Text"
+    CODE_TEXT = "Code Block"
+    LINK = "Link"
+    IMAGE = "Image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+
+    
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
